0bd7f89c194b91f191add8021b7ea1a6954531cd
wepsree <wepsree@pm.me>
2022-08-30 22:35:01 -0700

    bignumber.js

--- frontend/src/utils/strings.ts
+++ frontend/src/utils/strings.ts
@@ -1,3 +1,5 @@
+import BigNumber from 'bignumber.js';
+
 export const shortenString = (str: string, startCount = 8, endCount = 5) =>
 	str.slice(0, startCount) + '...' + str.slice(-endCount);
 export const shortenAddress = (address: string) => shortenString(address, 8, 5);
@@ -12,21 +14,11 @@ export const copyToClipboardAsync = (str = '') => {
 };
 
 export const toBiggestUnit = (num: string, decimals = 0) => {
-	// Assume num is a unsigned integer (i.e. positive with no decimals) and decimals is > 0
-	num = `${num.substring(0, num.length - decimals) || 0}.${'0'.repeat(
-		Math.max(0, decimals - num.length)
-	)}${num.substring(Math.max(0, num.length - decimals))}`;
-	return num.replace(/(0+|\.0+|\.)$/, '');
+	return new BigNumber(num).dividedBy(10 ** decimals).toFixed();
 };
 
 export const toSmallestUnit = (num: string, decimals = 0) => {
-	// Assume num is a positive number and decimals is > 0
-	const indexOfDot = num.indexOf('.');
-	if (indexOfDot === -1) {
-		return num + '0'.repeat(decimals);
-	}
-	const decimalPlaces = num.length - indexOfDot - 1;
-	return (num.substring(indexOfDot + 1) + '0'.repeat(decimals - decimalPlaces)).replace(/^0+/g, '');
+	return new BigNumber(num).multipliedBy(10 ** decimals).toFixed();
 };
 
 export const roundDownTo6Decimals = (balance: string) =>
