97cdee9c97f6da6d0e6ea95ed8b82d77fe84ae44
wepsree <wepsree@pm.me>
2022-09-22 19:13:25 -0700

    normalizeNumericInput

--- frontend/src/containers/TextInput.tsx
+++ frontend/src/containers/TextInput.tsx
@@ -42,14 +42,15 @@ type Props = State &
 		getIssue?: (value: string) => string | void;
 	};
 
-const normalizeNumericInput = (str: string, decimals = 6, removeInsignificantZeros = false) => {
+const normalizeNumericInput = (str: string, decimals: number, removeInsignificantZeros = false) => {
 	if (Number.isNaN(+str) || !str) {
 		return '';
 	}
-	const firstDotIndex = str.indexOf('.');
-	if (str.slice(firstDotIndex + 1).length > decimals) {
-		str = str.slice(0, firstDotIndex + decimals + 1);
+	let firstDotIndex = str.indexOf('.');
+	if (firstDotIndex === -1) {
+		firstDotIndex = str.length;
 	}
+	str = str.slice(0, firstDotIndex + decimals + 1);
 	if (removeInsignificantZeros) {
 		str = +str + '';
 	}
@@ -147,6 +148,7 @@ const TextInput = ({
 					internalValueSet(value);
 				}}
 				onChange={({ target: { value } }) => {
+					console.log('123e:', value);
 					// e.stopPropagation();
 					// e.preventDefault();
 					if (disabled) {
@@ -156,8 +158,8 @@ const TextInput = ({
 					if (numeric && value) {
 						// eslint-disable-next-line
 						value = value.replace(/[^0123456789\.]/g, '');
-						// value = value.replace(/\.+/g, '.');
 						value = normalizeNumericInput(value, maxDecimals);
+						console.log('value:', value);
 					}
 					value = maxLength ? value.slice(0, maxLength) : value;
 					if (onUserInput) {
